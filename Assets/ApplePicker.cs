d0e93e0beed4338350d898f8db15a8b44771bbbf
Nathaniel Tooley <gotablast99@gmail.com>
2024-02-04 16:26:51 -0600

    Changed buckets from 3 to 4

--- Assets/ApplePicker.cs
+++ Assets/ApplePicker.cs
@@ -1,5 +1,6 @@
 using System.Collections;
 using System.Collections.Generic;
+using Unity.VisualScripting;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 
@@ -10,34 +11,38 @@ public class ApplePicker : MonoBehaviour
     public int numBaskets = 3;
     public float basketBottomY = -14f;
     public float basketSpacingY = 2f;
-    public List<GameObject> basketList;
+    private List<GameObject> basketList;
     // Start is called before the first frame update
-    
+
     void Start()
     {
         basketList = new List<GameObject>();
-        for (int i = 0; i < numBaskets; i++){
+        for (int i = 0; i < numBaskets; i++)
+        {
             GameObject tBasketGO = Instantiate<GameObject>(basketPrefab);
             Vector3 pos = Vector3.zero;
             pos.y = basketBottomY + (basketSpacingY * i);
             tBasketGO.transform.position = pos;
             basketList.Add(tBasketGO);
         }
-        
+
     }
 
-    public void AppleDestroyed(){
+    public void AppleDestroyed()
+    {
         GameObject[] tAppleArray = GameObject.FindGameObjectsWithTag("Apple");
-        foreach (GameObject tGO in tAppleArray){
-            Destroy( tGO);
+        foreach (GameObject tGO in tAppleArray)
+        {
+            Destroy(tGO);
         }
 
-        int basketIndex = basketList.Count-1;
+        int basketIndex = basketList.Count - 1;
         GameObject tBasketGO = basketList[basketIndex];
         basketList.RemoveAt(basketIndex);
         Destroy(tBasketGO);
 
-        if (basketList.Count == 0) {
+        if (basketList.Count == 0)
+        {
             SceneManager.LoadScene("_Scene_0");
         }
     }
