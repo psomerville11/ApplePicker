e76e86da0bf4c4f34b1f85c3b565839ae6f71791
psomerville11 <parissymone06@gmail.com>
2024-02-07 21:14:14 -0600

    added Game Over screen and restart button

--- Assets/ApplePicker.cs
+++ Assets/ApplePicker.cs
@@ -59,7 +59,7 @@ public class ApplePicker : MonoBehaviour
 
         if (basketList.Count == 0)
         {
-            SceneManager.LoadScene("_Scene_0");
+            SceneManager.LoadScene("Game Over");
         }
         else
         {
---
+++ Assets/Scenes/Restart.cs
@@ -0,0 +1,22 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class Restart : MonoBehaviour
+{
+
+    void Start()
+  {
+     GetComponent<Button>().onClick.AddListener(RestartGame);
+   }
+    
+    void RestartGame()
+    {
+        SceneManager.LoadScene("_Scene_0");
+        
+    }
+
+    
+}
