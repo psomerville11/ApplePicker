edf8a6b3061e8997db3da30acba28bcc595b4997
Nathaniel Tooley <gotablast99@gmail.com>
2024-02-08 15:22:29 -0600

    Formatting

--- Assets/Scenes/Restart.cs
+++ Assets/Scenes/Restart.cs
@@ -7,16 +7,16 @@ using UnityEngine.UI;
 public class Restart : MonoBehaviour
 {
 
-    void Start()
+  void Start()
   {
-     GetComponent<Button>().onClick.AddListener(RestartGame);
-   }
-    
-    void RestartGame()
-    {
-        SceneManager.LoadScene("_Scene_0");
-        
-    }
+    GetComponent<Button>().onClick.AddListener(RestartGame);
+  }
 
-    
+  void RestartGame()
+  {
+    SceneManager.LoadScene("_Scene_0");
+
+  }
+
+
 }
